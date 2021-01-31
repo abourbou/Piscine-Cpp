c4c9bd2f7fd4caeeb97d8d132e1539cf5f895d3a
arthur6996 <arthuro6996@live.fr>
2021-01-31 21:46:25 +0100

    start of ex07

---
+++ C01/ex07/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abourbou <abourbou@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/31 21:36:33 by abourbou          #+#    #+#             */
+/*   Updated: 2021/01/31 21:45:49 by abourbou         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+
+int		main(int argc)
+{
+	
+
+	if (argc != 4)
+	{
+		std::cout << "Wrong number of arguments" << std::endl;
+		return (0);
+	}
+
+}
