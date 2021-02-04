ff7b48609ee8ad82ab2c5757f14c9241132152dd
arthur6996 <arthuro6996@live.fr>
2021-02-04 22:56:47 +0100

    finished C02 ex01

--- C02/ex01/Fixed.cpp
+++ C02/ex01/Fixed.cpp
@@ -6,12 +6,13 @@
 /*   By: abourbou <abourbou@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/03 21:36:12 by abourbou          #+#    #+#             */
-/*   Updated: 2021/02/04 22:38:42 by abourbou         ###   ########lyon.fr   */
+/*   Updated: 2021/02/04 22:55:31 by abourbou         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 #include <iostream>
+#include <cmath>
 
 Fixed::Fixed(void) : _value(0)
 {
@@ -22,6 +23,16 @@ Fixed::Fixed(Fixed const &cpy)
 {
 	std::cout << "Copy Constructor called" << std::endl;
 	*this = cpy;
+}
+
+Fixed::Fixed(int const integer) : _value(integer << _width)
+{
+	std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(float const floated) : _value(roundf(floated * 256.0))
+{
+	std::cout << "Float constructor called" << std::endl;
 }
 
 Fixed::~Fixed(void)
@@ -46,4 +57,20 @@ void	Fixed::setRawBits(int const raw)
 {
 	std::cout << "setRawBits member function called" << std::endl;
 	this->_value = raw;
+}
+
+float	Fixed::toFloat(void) const
+{
+	return ((float)_value / 256.0);
+}
+
+int		Fixed::toInt(void) const
+{
+	return (_value >> _width);
+}
+
+std::ostream	& operator<<(std::ostream &o, Fixed const &rhs)
+{
+	o << rhs.toFloat();
+	return (o);
 }
--- C02/ex01/Fixed.hpp
+++ C02/ex01/Fixed.hpp
@@ -6,12 +6,14 @@
 /*   By: abourbou <abourbou@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/03 21:34:04 by abourbou          #+#    #+#             */
-/*   Updated: 2021/02/04 22:37:27 by abourbou         ###   ########lyon.fr   */
+/*   Updated: 2021/02/04 22:53:57 by abourbou         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
 # define FIXED_HPP
+
+#include <iostream>
 
 class Fixed
 {
@@ -22,12 +24,18 @@ class Fixed
 	public:
 		Fixed(void);
 		Fixed(Fixed const &cpy);
+		Fixed(int const integer);
+		Fixed(float const floated);
 		~Fixed(void);
 
 		Fixed	&operator=(Fixed const &cpy);
 
 		int		getRawBits(void) const;
 		void	setRawBits(int const raw);
+		float	toFloat(void) const;
+		int		toInt(void) const;
 };
+
+std::ostream	&operator<<(std::ostream &o, Fixed const &rhs);
 
 #endif
